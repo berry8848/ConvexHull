1f047a201fe701107e97cdc6c204c76a05aeccbd
Watanabe Ryunosuke <rwatanabe20@ixas.unisys.co.jp>
2025-01-16 17:46:39 +0900

    0116_work

---
+++ src/TestDll.cpp
@@ -0,0 +1,33 @@
+// QhullTest.cpp : Defines the entry point for the console application.
+//
+
+// #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "StlFile.h"
+#include "ConvexHull.h"
+#include "TestDll.h"
+
+#include <libqhullcpp\Qhull.h>
+#include <libqhullcpp\QhullError.h>
+
+extern "C" EXPORT void TestDll()
+{
+	std::string outputFile = "C:/myFolder/ConvexHull/data/Output.stl";
+
+	StlFile stl("C:/myFolder/ConvexHull/data/Nut.stl");
+
+	ConvexHull convex;
+	convex.setVertexes(stl.vertexes());
+	try {
+		convex.execute();
+	}
+	catch(orgQhull::QhullError &e){
+		std::cerr << e.what() << std::endl;
+	}
+
+	StlFile outStl;
+	outStl.setVertexes(convex.convexVertexes());
+	outStl.setNormals(convex.convexNormals());
+	outStl.write(outputFile.c_str());
+}
